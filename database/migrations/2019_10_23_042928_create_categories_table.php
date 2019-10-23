044d223547b34f2602d3a3712fd66338c4b5bb90
SamboSovan011 <lucksolent@gmail.com>
2019-10-23 11:39:03 +0700

    fix migration file

---
+++ database/migrations/2019_10_23_042928_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('user_email', 100);
+            $table->string('img_path', 20);
+            $table->string('title', 100);
+            $table->string('description', 800);
+            $table->integer('is_approved')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
