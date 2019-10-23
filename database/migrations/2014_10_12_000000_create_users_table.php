135a9b9cafc2a58e04576742ef0065e9e6a7587a
SamboSovan011 <lucksolent@gmail.com>
2019-10-23 22:40:34 +0700

    migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('fname');
+            $table->string('lname');
+            $table->integer('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('is_admin')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
